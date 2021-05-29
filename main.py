40be08f5a8ab978951752c5b2f3548f01b191c3b
Jonah Ortiz <>
2021-05-29 02:26:56 +0000

    frtvvvvvvvv

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Your Savings Calculator")
 user_input = input("How much do you make per hour ")
-print(user_input)
+daily_savings = int(user_input) * 8
+print("This is your daily savings", daily_savings)
+weekly_savings = daily_savings * 5
+print("this is your weekly savings", weekly_savings)
+monthly_savings = weekly_savings * 5
+print("this is your monthly savings", weekly_savings)
