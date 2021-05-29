6cf1f10970fcd3e113f1c846bbd97232f26df557
Jonah Ortiz <>
2021-05-29 02:58:33 +0000

    ojnlkm,

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ daily_savings = int(user_input) * 8
 print("This is your daily savings", daily_savings)
 weekly_savings = daily_savings * 5
 print("this is your weekly savings", weekly_savings)
-monthly_savings = weekly_savings * 5
+monthly_savings = weekly_savings * 8
 print("this is your monthly savings", weekly_savings)
+print("Yay! you make", monthly_savings)
+print("per month")
+print("you can know click stop or the x in the calculator")
+print("You can see my website at https://sites.google.com/sbcusd.k12.ca.us/jonah/home")
